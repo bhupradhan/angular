2d21896cdac133f4da70976803085543b652539e
bhupradhan <58717082+bhupradhan@users.noreply.github.com>
2020-05-27 17:55:55 +0530

    config angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { MatToolbarModule } from '@angular/material/tookbar';
+import { FlexLayoutModule} from '@angular/flex-layout';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
